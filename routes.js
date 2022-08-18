b2664bb7c49398ff3e177c23c1bf40f71faf183f
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2022-08-18 16:19:08 +0530

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,15 +3,4 @@
 
 import { Router } from '@layer0/core/router'
 
-export default new Router().static('assets', ({ cache }) => {
-  cache({
-    edge: {
-      maxAgeSeconds: 60 * 60 * 60 * 365,
-      forcePrivateCaching: true,
-    },
-    browser: {
-      maxAgeSeconds: 0,
-      serviceWorkerSeconds: 60 * 60 * 24,
-    },
-  })
-})
+export default new Router().static('assets')
